17198c740fb251582a5e4877f57c9d58d7aa3f0f
Samiksha Danole <samdanole123@gmail.com>
2025-12-10 22:24:47 +0530

    add sub

---
+++ Arithmetic.py
@@ -0,0 +1,7 @@
+
+
+def add(a ,b):
+    print("Addition Is :",a+b)
+
+def sub(a,b):
+    print("Subtraction Is :",a-b)    
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+import Arithmetic as ab
+a=int(input("enter num1"))
+b=int(input("enter num2"))
+ab.add(a,b)
+ab.sub(a,b)
 print("Hello World")
