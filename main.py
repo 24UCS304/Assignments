058327da790d1c37adb83ab1ef000d3e6b7ada75
Samiksha Danole <samdanole123@gmail.com>
2025-12-11 22:02:42 +0530

    day 1 assignment

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import Arithmetic as ab
+import geometry as g
+
 a=int(input("enter num1"))
 b=int(input("enter num2"))
 ab.add(a,b)
 ab.sub(a,b)
+
+g.rec_area(a,b)
+g.rec_peri(a,b)
+
 print("Hello World")
